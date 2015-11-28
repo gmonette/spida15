5f748461ed31514d1e87e954ba7140a29c31385a
Georges Monette <georges@yorku.ca>
2015-11-28 16:10:30 -0500

    export gd

--- R/fun.R
+++ R/fun.R
@@ -1023,116 +1023,7 @@ td <- function(
 	pch = 1:7, cex = 0.8, font = 1,
 
 
-#' Create a time-invariant variable by filling in NAs -- generic
-#' 
-#' %% ~~ A concise (1-5 lines) description of what the function does. ~~
-#' 
-#' %% ~~ If necessary, more details than the description above ~~
-#' 
-#' @param x %% ~~Describe \code{x} here~~
-#' @param \dots %% ~~Describe \code{\dots} here~~
-#' @note %% ~~further notes~~
-#' @author %% ~~who you are~~
-#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
-#' @references %% ~put references to the literature/web site here ~
-#' @keywords ~kwd1 ~kwd2
-#' @examples
-#' 
-#' ##---- Should be DIRECTLY executable !! ----
-#' ##-- ==>  Define data, use random,
-#' ##--	or do  help(data=index)  for the standard data sets.
-#' 
-#' ## The function is currently defined as
-#' function (x, ...) 
-#' UseMethod("fill")
-#' 
-	fill = "transparent",
-	col.line = col,
-	col.symbol = col,
-	alpha = 1,
-	alpha.line = alpha,
-	alpha.symbol = alpha,
-	len = 7,
 
-
-#' Create a longitudinal file from a wide file
-#' 
-#' %% ~~ A concise (1-5 lines) description of what the function does. ~~
-#' 
-#' %% ~~ If necessary, more details than the description above ~~
-#' 
-#' @aliases long tolong
-#' @param data %% ~~Describe \code{data} here~~
-#' @param sep (default "_" in 'tolong") the separator between the long variable
-#' name and the time
-#' @param expand (default FALSE in 'tolong') if TRUE and if there are missing
-#' combinations of long variable names and times, the missing combinations are
-#' created with NAs
-#' @param varying %% ~~Describe \code{varying} here~~
-#' @param sep.varying %% ~~Describe \code{sep.varying} here~~
-#' @param v.names %% ~~Describe \code{v.names} here~~
-#' @param timevar %% ~~Describe \code{timevar} here~~
-#' @param idvar %% ~~Describe \code{idvar} here~~
-#' @param ids %% ~~Describe \code{ids} here~~
-#' @param times %% ~~Describe \code{times} here~~
-#' @param drop %% ~~Describe \code{drop} here~~
-#' @param new.row.names %% ~~Describe \code{new.row.names} here~~
-#' @param split %% ~~Describe \code{split} here~~
-#' @param debug %% ~~Describe \code{debug} here~~
-#' @note %% ~~further notes~~
-#' @author %% ~~who you are~~
-#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
-#' @references %% ~put references to the literature/web site here ~
-#' @keywords ~kwd1 ~kwd2
-#' @examples
-#' 
-#' \dontrun{
-#' ##---- Should be DIRECTLY executable !! ----
-#' ##-- ==>  Define data, use random,
-#' ##--	or do  help(data=index)  for the standard data sets.
-#' 
-#' ## The function is currently defined as
-#' function (data, varying = NULL, sep.varying = "\.", v.names = names(Varying),
-#'     timevar = "time", idvar = "id", ids = 1:NROW(data), times = seq(length = length(Varying[[1]])),
-#'     drop = NULL, new.row.names = NULL, split = list(regexp = "\.",
-#'         include = FALSE), debug = FALSE)
-#' {
-#'     help <- "\n    Example:   long(dwl, varying = c('wmrss', 'lm1tot','lm2tot'), sep='')\n    "
-#'     nn <- names(data)
-#'     if (debug)
-#'         disp(nn)
-#'     Varying <- list()
-#'     for (ii in 1:length(varying)) {
-#'         if (length(prefix <- varying[[ii]]) == 1) {
-#'             ns <- grep(paste("^", prefix, sep.varying, sep = ""),
-#'                 nn, value = T)
-#'             if (debug)
-#'                 disp(ns)
-#'             Varying[[ii]] <- ns
-#'             names(Varying)[ii] <- prefix
-#'         }
-#'         else {
-#'             Varying[[ii]] <- varying[[ii]]
-#'             names(Varying)[ii] <- names(varying)[ii]
-#'         }
-#'     }
-#'     if (debug)
-#'         disp(Varying)
-#'     if (debug)
-#'         disp(times)
-#'     ret <- stats::reshape(data, Varying, v.names, timevar, idvar,
-#'         ids, times, drop, direction = "long", new.row.names,
-#'         split)
-#'     ret[order(ret[[idvar]], ret[[timevar]]), ]
-#'   }
-#' 
-#' 
-#' 
-#' tolong <- function(data, sep = "_",...){
-#'   reshape(data, direction = 'long', sep = sep, varying = grep(sep, names(data)),...)
-#' }
-#' }
-#' 
 	long = FALSE,
 #    record = FALSE, # not supported in RStudio
     basecol = NULL,
@@ -1258,6 +1149,7 @@ td <- function(
   invisible(ret[grep('superpose',names(ret))])
 }
 
+#' @export
 gd <-
   function (n=9, 
             col = brewer.pal(n,"Set1"), lty = 1:n, lwd = 1, 
